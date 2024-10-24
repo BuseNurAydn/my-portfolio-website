f01fdb2e353af278b2b2e1d901f975860a3a401a
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-10-24 21:10:08 +0300

    düzenlemeler ve eklemeler yapılıyor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-/** js kodlar yazılacak
- * 
- */
+/** js kodlar yazılacak */
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.card');
+    const totalSlides = slides.length;
+
+
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = Math.ceil(totalSlides / 4) -1 ; // Son gruba git
+    } else if (currentSlide >= Math.ceil(totalSlides / 4)) {
+        currentSlide = 0; // İlk gruba dön
+    }
+    const carouselContainer = document.querySelector('.carousel-container');
+    carouselContainer.style.transform = `translateX(-${currentSlide * (100 / 4)}%)`; // 4 kart gösterdiğimiz için
+}
+setInterval(() => {
+    moveSlide(1);
+}, 5000); // 5 saniyede bir otomatik kaydırma
